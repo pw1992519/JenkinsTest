96862441be47991f63906a214b47fb25f6cdcc61
apple <30474773@qq.com>
2020-01-07 17:25:01 +0800

    d

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const Koa = require('koa');
 const app = new Koa();
+const _ = require('lodash');
 
 app.use(async ctx => {
   ctx.body = 'Hello World1';
 }); 
-console.log('aaaa')  
+console.log(_.last([1, 2, 3]))  
 
 // app.listen(3000);
